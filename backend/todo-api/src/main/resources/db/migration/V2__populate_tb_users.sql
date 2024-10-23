c3b9b94024452669ab735a4e679caf7e7d43f8f8
John Rodrigues <jocimarrodrigues1337@gmail.com>
2024-10-23 01:34:22 -0300

    feat(adicionando flyway no projeto, criando 1 migration)

---
+++ backend/todo-api/src/main/resources/db/migration/V2__populate_tb_users.sql
@@ -0,0 +1,8 @@
+INSERT INTO tb_users (name, password, email) VALUES
+('Alex', '123213', 'wvqgI@example.com'),
+('Alex2', '435345', 'alex@example.com'),
+('Pedro', '654645', 'pedro@example.com'),
+('Joao', '12326546', 'joao@example.com'),
+('Carlos', '45435', 'carlos@example.com');
+
+COMMIT;
