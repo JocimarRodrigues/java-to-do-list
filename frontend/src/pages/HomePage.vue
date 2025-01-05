2b269b85af4aadc89346ceadd72284c3a92d7ffe
John Rodrigues <jocimarrodrigues1337@gmail.com>
2025-01-04 22:07:15 -0300

    style(responsividade para home)

--- frontend/src/pages/HomePage.vue
+++ frontend/src/pages/HomePage.vue
@@ -1,90 +1,88 @@
 <template>
-  <div>
-    <q-page class="">
-      <div
-        class="row q-pa-md q-gutter-md justify-center items-center w-full h-[95dvh]"
+  <q-page>
+    <div
+      class="row q-pa-md q-gutter-md justify-center items-center w-full h-[95dvh]"
+    >
+      <q-card
+        class="bg-white w-[90vw] md:w-[60vw] h-[90vh] md:h-[70vh] rounded-lg flex flex-col md:flex-row flex-nowrap q-pa-sm"
       >
-        <q-card
-          class="bg-white w-[60vw] h-[70vh] rounded-lg flex flex-nowrap q-pa-sm"
-        >
-          <div class="w-[50%] h-[98%] m-2">
-            <q-img :src="`${bgImage}`" class="h-full rounded" />
-          </div>
-          <div class="flex flex-col w-[50%] flex-nowrap">
-            <q-form
-              class="flex flex-col items-center justify-center h-full flex-nowrap q-pa-md"
-              @submit.prevent="onSubmit"
-            >
-              <h1 class="text-3xl mt-4">
-                {{ isCreate ? 'Criar Conta' : 'Login' }}
-              </h1>
-              <div class="q-pa-md w-full">
-                <q-input
-                  class="my-4"
-                  label="Nome"
-                  placeholder="Digite seu nome..."
-                  outlined
-                  dense
-                  v-model="user.name"
-                />
-                <q-input
-                  class="my-4"
-                  label="Email"
-                  type="email"
-                  placeholder="Digite seu email..."
-                  outlined
-                  dense
-                  v-model="user.email"
-                  :rules="[(val: string) => !!val || 'Campo obrigatório!']"
-                />
-                <q-input
-                  label="Senha"
-                  placeholder="Digite sua senha..."
-                  outlined
-                  dense
-                  v-model="user.password"
-                />
-              </div>
-              <template v-if="errors">
-                <span
-                  v-for="item in errors"
-                  :key="item"
-                  class="text-red-600 font-bold"
-                >
-                  {{ item }}
-                </span>
-              </template>
-              <div class="flex flex-col q-pa-md w-full gap-4">
-                <q-btn
-                  v-if="!isCreate"
-                  label="Entrar"
-                  color="black"
-                  no-caps
-                  class="font-medium"
-                  type="submit"
-                />
-                <q-btn
-                  v-else
-                  label="Enviar"
-                  color="black"
-                  no-caps
-                  class="font-medium"
-                  type="submit"
-                />
-                <p
-                  class="text-center w-full text-blue-400 cursor-pointer"
-                  v-if="!isCreate"
-                  @click="() => (isCreate = !isCreate)"
-                >
-                  Não tem uma conta? Clique aqui para se cadastar.
-                </p>
-              </div>
-            </q-form>
-          </div>
-        </q-card>
-      </div>
-    </q-page>
-  </div>
+        <div class="h-[40%] md:w-[50%] md:h-[98%] m-2">
+          <q-img :src="`${bgImage}`" class="q-pa-md h-full rounded" />
+        </div>
+        <div class="flex flex-col w-full md:w-[50%] flex-nowrap">
+          <q-form
+            class="flex flex-col items-center justify-center h-full flex-nowrap q-pa-md"
+            @submit.prevent="onSubmit"
+          >
+            <h1 class="text-3xl mt-4">
+              {{ isCreate ? 'Criar Conta' : 'Login' }}
+            </h1>
+            <div class="q-pa-md w-full">
+              <q-input
+                class="my-4"
+                label="Nome"
+                placeholder="Digite seu nome..."
+                outlined
+                dense
+                v-model="user.name"
+              />
+              <q-input
+                class="my-4"
+                label="Email"
+                type="email"
+                placeholder="Digite seu email..."
+                outlined
+                dense
+                v-model="user.email"
+                :rules="[(val: string) => !!val || 'Campo obrigatório!']"
+              />
+              <q-input
+                label="Senha"
+                placeholder="Digite sua senha..."
+                outlined
+                dense
+                v-model="user.password"
+              />
+            </div>
+            <template v-if="errors">
+              <span
+                v-for="item in errors"
+                :key="item"
+                class="text-red-600 font-bold"
+              >
+                {{ item }}
+              </span>
+            </template>
+            <div class="flex flex-col q-pa-md w-full gap-4">
+              <q-btn
+                v-if="!isCreate"
+                label="Entrar"
+                color="black"
+                no-caps
+                class="font-medium"
+                type="submit"
+              />
+              <q-btn
+                v-else
+                label="Enviar"
+                color="black"
+                no-caps
+                class="font-medium"
+                type="submit"
+              />
+              <p
+                class="text-center w-full text-blue-400 cursor-pointer"
+                v-if="!isCreate"
+                @click="() => (isCreate = !isCreate)"
+              >
+                Não tem uma conta? Clique aqui para se cadastar.
+              </p>
+            </div>
+          </q-form>
+        </div>
+      </q-card>
+    </div>
+  </q-page>
 </template>
 <script setup lang="ts">
 import bgImage from 'src/assets/images/bg2.jpg';
